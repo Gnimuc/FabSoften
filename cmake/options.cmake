d1418a1c7b5d8529ace0431290f8d0ef83487627
Gnimuc <qiyupei@gmail.com>
2021-10-26 00:47:34 +0900

    Add prefix to CMake options

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,3 +1,3 @@
-option(BUILD_TESTS "Build Tools" ON)
-option(BUILD_TOOLS "Build Unit Tests" ON)
-option(BUILD_EXAMPLES "Build Examples" ON)
+option(FABSOFTEN_BUILD_TESTS "Build Tools" ON)
+option(FABSOFTEN_BUILD_TOOLS "Build Unit Tests" ON)
+option(FABSOFTEN_BUILD_EXAMPLES "Build Examples" ON)
