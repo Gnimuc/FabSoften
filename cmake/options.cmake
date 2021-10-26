e1497a9a0dcac3715d9cbc50fae587ae0238310e
Gnimuc <qiyupei@gmail.com>
2021-10-26 19:40:50 +0900

    Pass `--verbose` to `ctest` for getting the Catch2 test report

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,3 +1,3 @@
-option(FABSOFTEN_BUILD_TESTS "Build Tools" ON)
-option(FABSOFTEN_BUILD_TOOLS "Build Unit Tests" ON)
+option(FABSOFTEN_BUILD_TOOLS "Build Tools" ON)
+option(FABSOFTEN_BUILD_TESTS "Build Unit Tests" ON)
 option(FABSOFTEN_BUILD_EXAMPLES "Build Examples" ON)
