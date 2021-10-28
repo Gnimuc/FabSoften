e22f74cfe8404d43d71dd3d0bda84812b4d2b5a5
Gnimuc <qiyupei@gmail.com>
2021-10-28 20:18:37 +0900

    Build docs with `doxygen`

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,3 +1,4 @@
 option(FABSOFTEN_BUILD_TOOLS "Build Tools" ON)
 option(FABSOFTEN_BUILD_TESTS "Build Unit Tests" ON)
 option(FABSOFTEN_BUILD_EXAMPLES "Build Examples" ON)
+option(FABSOFTEN_BUILD_DOCS "Build Docs" OFF)
