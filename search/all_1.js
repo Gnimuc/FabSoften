08ac1f8fff4dcdca9a4c78542df43752d9820cb8
Gnimuc <Gnimuc@users.noreply.github.com>
2021-10-30 07:47:46 +0000

    deploy: 384d6506011c81ae5641feddeaf549e7bb04fae2

--- search/all_1.js
+++ search/all_1.js
@@ -6,5 +6,5 @@ var searchData=
   ['fabsoften_5flinkage_3',['FABSOFTEN_LINKAGE',['../_platform_8h.html#a644f2c5bdb8ade3b9eeab18dd1cb3bf7',1,'Platform.h']]],
   ['fabsoften_5fsanity_5fcheck_4',['fabsoften_sanity_check',['../_lib_fab_soften_8h.html#a4ebfee04bff98bcd2361dc3b9dddedc3',1,'fabsoften_sanity_check(void):&#160;LibFabSoften.cpp'],['../_lib_fab_soften_8cpp.html#ad4785ff9d0ffd5e1098befd6f526d5be',1,'fabsoften_sanity_check(void):&#160;LibFabSoften.cpp']]],
   ['face_20landmark_20detection_5',['Face Landmark Detection',['../md_examples__face_landmark_detection__r_e_a_d_m_e.html',1,'']]],
-  ['face_5flandmark_5fdetection_2ecpp_6',['face_landmark_detection.cpp',['../face__landmark__detection_8cpp.html',1,'']]]
+  ['facelandmarkdetection_2ecpp_6',['FaceLandmarkDetection.cpp',['../_face_landmark_detection_8cpp.html',1,'']]]
 ];
--- search/functions_1.js
+++ search/functions_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['main_0',['main',['../face__landmark__detection_8cpp.html#a3c04138a5bfe5d72780bb7e82a18e627',1,'face_landmark_detection.cpp']]]
+  ['main_0',['main',['../_face_landmark_detection_8cpp.html#a3c04138a5bfe5d72780bb7e82a18e627',1,'FaceLandmarkDetection.cpp']]]
 ];
