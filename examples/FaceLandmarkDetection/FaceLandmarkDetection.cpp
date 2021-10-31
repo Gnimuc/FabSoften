78758e464c23f0e62aa970b5daeb206d8d438118
Gnimuc <qiyupei@gmail.com>
2021-10-31 14:26:26 +0900

    Add more docstrings

--- examples/FaceLandmarkDetection/FaceLandmarkDetection.cpp
+++ examples/FaceLandmarkDetection/FaceLandmarkDetection.cpp
@@ -1,7 +1,7 @@
 /**
  * @file FaceLandmarkDetection.cpp
  * @brief An example of how to do face landmark detection with OpenCV and dlib.
- *
+ * 
  */
 
 #include <dlib/image_processing.h>
@@ -13,7 +13,7 @@
 #include <opencv2/imgproc.hpp>
 #include <ranges>
 
-/*Command line keys for command line parsing*/
+/// @brief Command line keys for command line parsing
 static constexpr auto cmdKeys =
     "{help h usage ?   |       | print this message            }"
     "{@image           |<none> | input image                   }"
@@ -21,10 +21,14 @@ static constexpr auto cmdKeys =
     "{images_dir       |       | search path for images        }"
     "{models_dir       |       | search path for models        }";
 
-/*Default window name*/
-static constexpr auto windowName = "FaceLandmarkDetection";
+/// @brief Face Landmark Detection Window
+static constexpr auto landmarkWin = "FaceLandmarkDetection";
 
+/// @brief Face landmark detection example
+///
+/// Usage: FaceLandmarkDetection.exe [params] image landmark_model
 int main(int argc, char **argv) {
+  // Handle command line arguments
   cv::CommandLineParser parser(argc, argv, cmdKeys);
 
   parser.about("Face landmark detection example");
@@ -47,6 +51,8 @@ int main(int argc, char **argv) {
     parser.printMessage();
     return -1;
   }
+  // Set `required=false` to prevent `findFile` from throwing an exception.
+  // Instead, we check whether the image is valid via the `empty` method.
   auto inputImg =
       cv::imread(cv::samples::findFile(imgArg, /*required=*/false, /*silentMode=*/true));
   if (inputImg.empty()) {
@@ -57,14 +63,14 @@ int main(int argc, char **argv) {
     return -1;
   }
 
-  // Load models
+  // Load dlib's face landmark detection model
   auto landmarkModelArg = parser.get<cv::String>("@landmark_model");
   if (!parser.check()) {
     parser.printErrors();
     parser.printMessage();
     return -1;
   }
-  auto landmarkModelPath = cv::samples::findFile(landmarkModelArg, /*required=*/true);
+  auto landmarkModelPath = cv::samples::findFile(landmarkModelArg, /*required=*/false);
   if (landmarkModelPath.empty()) {
     std::cout << "Could not find the landmark model file: " << landmarkModelArg << "\n"
               << "The model should be located in `models_dir`.\n"
@@ -72,14 +78,15 @@ int main(int argc, char **argv) {
     parser.printMessage();
     return -1;
   }
-
+  
   dlib::shape_predictor landmarkDetector;
   dlib::deserialize(landmarkModelPath) >> landmarkDetector;
 
   // Detect faces
+  // Need to use `dlib::cv_image` to bridge OpenCV and dlib.
   const auto dlibImg = dlib::cv_image<dlib::bgr_pixel>(inputImg);
   auto faceDetector = dlib::get_frontal_face_detector();
-  auto faces = faceDetector(dlibImg);
+  auto faces = faceDetector(dlibImg);  // FIXME: see issue #1
 
   // Detect and draw landmarks
   const auto detect = [&](const auto &face) { return landmarkDetector(dlibImg, face); };
@@ -95,13 +102,13 @@ int main(int argc, char **argv) {
   }
 
   // Fit image to the screen and show image
-  cv::namedWindow(windowName, cv::WINDOW_NORMAL);
-  cv::setWindowProperty(windowName, cv::WND_PROP_FULLSCREEN, cv::WINDOW_FULLSCREEN);
-  auto [x, y, resW, resH] = cv::getWindowImageRect(windowName);
+  cv::namedWindow(landmarkWin, cv::WINDOW_NORMAL);
+  cv::setWindowProperty(landmarkWin, cv::WND_PROP_FULLSCREEN, cv::WINDOW_FULLSCREEN);
+  auto [x, y, resW, resH] = cv::getWindowImageRect(landmarkWin);
   auto [imgW, imgH] = inputImg.size();
   const auto scale = 40;
-  cv::resizeWindow(windowName, scale * resW / 100, scale * imgH * resW / (imgW * 100));
-  cv::imshow(windowName, inputImg);
+  cv::resizeWindow(landmarkWin, scale * resW / 100, scale * imgH * resW / (imgW * 100));
+  cv::imshow(landmarkWin, inputImg);
   cv::waitKey();
   cv::destroyAllWindows();
 
--- examples/FaceLandmarkDetection/README.md
+++ examples/FaceLandmarkDetection/README.md
@@ -1,5 +1,8 @@
 # Face Landmark Detection
 
+This example demonstrates how to use OpenCV and dlib for face landmark detection.
+
 ## References
 
--  https://github.com/davisking/dlib/blob/master/examples/face_landmark_detection_ex.cpp
+- https://github.com/davisking/dlib/blob/master/examples/face_landmark_detection_ex.cpp
+- https://docs.opencv.org/4.5.3/d0/d2e/classcv_1_1CommandLineParser.html#details
