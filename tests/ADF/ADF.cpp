a286254b73d03b9c6fbf02815d471daf2683fb40
Gnimuc <qiyupei@gmail.com>
2021-11-15 01:10:34 +0900

    Add unit-tests for ADF

---
+++ tests/ADF/ADF.cpp
@@ -0,0 +1,65 @@
+#include "ADF.h"
+#include <catch2/catch_test_macros.hpp>
+#include <iostream>
+#include <opencv2/imgproc.hpp>
+
+TEST_CASE("ADF", "[integral image]") {
+  cv::Mat img(100, 100, CV_8UC3);
+  cv::randu(img, cv::Scalar(0, 0, 0), cv::Scalar(255, 255, 255));
+
+  cv::Mat integralImg;
+  cv::integral(img, integralImg);
+
+  SECTION("dimension") {
+    REQUIRE(integralImg.rows == img.rows + 1);
+    REQUIRE(integralImg.cols == img.cols + 1);
+  }
+
+  SECTION("summed-area table") {
+    for (size_t x = 0; x < img.rows; ++x)
+      for (size_t y = 0; y < img.cols; ++y) {
+        const auto pA = cv::Point(y, x);
+        const auto pB = cv::Point(y + 1, x);
+        const auto pC = cv::Point(y, x + 1);
+        const auto pD = cv::Point(y + 1, x + 1);
+        const auto &sA = integralImg.at<cv::Vec3i>(pA);
+        const auto &sB = integralImg.at<cv::Vec3i>(pB);
+        const auto &sC = integralImg.at<cv::Vec3i>(pC);
+        const auto &sD = integralImg.at<cv::Vec3i>(pD);
+        const auto sum = sD + sA - sB - sC;
+        const auto &val = img.at<cv::Vec3b>(x, y);
+        REQUIRE(cv::Vec3i(val) == sum);
+      }
+  }
+
+  constexpr auto r = 2; // radius
+  const auto blockSize = cv::Size(2 * r + 1, 2 * r + 1);
+  cv::Mat boxImg;
+  cv::boxFilter(img, boxImg, /*ddepth=*/-1, blockSize);
+
+  const auto nRow = integralImg.rows, nCol = integralImg.cols;
+  constexpr auto border = cv::BORDER_DEFAULT;
+  SECTION("boxfilter") {
+    cv::Mat meanImg = cv::Mat::zeros(img.size(), CV_8UC3);
+    for (size_t x = 0; x < img.rows; ++x)
+      for (size_t y = 0; y < img.cols; ++y) {
+        const auto &sA =
+            integralImg.at<cv::Vec3i>(cv::borderInterpolate(x - r, nRow, border),
+                                      cv::borderInterpolate(y - r, nCol, border));
+        const auto &sB =
+            integralImg.at<cv::Vec3i>(cv::borderInterpolate(x - r, nRow, border),
+                                      cv::borderInterpolate(y + r + 1, nCol, border));
+        const auto &sC =
+            integralImg.at<cv::Vec3i>(cv::borderInterpolate(x + r + 1, nRow, border),
+                                      cv::borderInterpolate(y - r, nCol, border));
+        const auto &sD =
+            integralImg.at<cv::Vec3i>(cv::borderInterpolate(x + r + 1, nRow, border),
+                                      cv::borderInterpolate(y + r + 1, nCol, border));
+        meanImg.at<cv::Vec3b>(x, y) = (sD + sA - sB - sC) / ((2 * r + 1) * (2 * r + 1));
+      }
+
+    for (size_t x = r; x < img.rows - r; ++x)
+      for (size_t y = r; y < img.cols - r; ++y)
+        REQUIRE(boxImg.at<cv::Vec3b>(x, y) == meanImg.at<cv::Vec3b>(x, y));
+  }
+}
---
+++ tests/ADF/ADF.h
@@ -0,0 +1,6 @@
+#ifndef ADF_H
+#define ADF_H
+
+#include "fabsoften/LibFabSoften.h"
+
+#endif
