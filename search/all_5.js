ab3c11a60a2b85f94d5a44098788d6bb532cf55a
Gnimuc <Gnimuc@users.noreply.github.com>
2021-11-24 14:22:08 +0000

    deploy: 5051fd65c0a508db230cf4b3ab05f0f09fee7cc3

--- search/all_5.js
+++ search/all_5.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['fabsoften_0',['fabsoften',['../namespacefabsoften.html',1,'']]],
-  ['fabsoften_1',['FabSoften',['../md__r_e_a_d_m_e.html',1,'']]],
+  ['fabsoften_1',['FabSoften',['../index.html',1,'']]],
   ['fabsoften_5fdeprecated_2',['FABSOFTEN_DEPRECATED',['../_platform_8h.html#a9c394ceb2e562f119c9b7bde1556f39f',1,'Platform.h']]],
   ['fabsoften_5fexports_3',['FABSOFTEN_EXPORTS',['../_platform_8h.html#a6c1c4f8c0ce0be3ba6beb7ae96fadecb',1,'Platform.h']]],
   ['fabsoften_5flinkage_4',['FABSOFTEN_LINKAGE',['../_platform_8h.html#a644f2c5bdb8ade3b9eeab18dd1cb3bf7',1,'Platform.h']]],
